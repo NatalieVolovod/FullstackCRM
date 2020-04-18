305ed966de0fcb31979a1e0b060f06ae7198529e
NatalieVolovod <volovod.natalie@gmail.com>
2020-04-18 21:11:17 +0300

    categories functionality

--- routes/category.js
+++ routes/category.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const upload = require('../middleware/upload');
 const router = express.Router();
 const controller = require('../controllers/category');
 
-// localhost:5000/api/category
-router.get('/', controller.getAll);
-
-// localhost:5000/api/category/:id
-router.get('/:id', controller.getById);
-
-// localhost:5000/api/category/:id
-router.delete('/:id', controller.remove);
 
 // localhost:5000/api/category
-router.post('/', controller.create);
+router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
 
 // localhost:5000/api/category/:id
-router.patch('/:id', controller.update);
+router.get('/:id', passport.authenticate('jwt', {session: false}), controller.getById);
+
+// localhost:5000/api/category/:id
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
+
+// localhost:5000/api/category
+router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.create);
+
+// localhost:5000/api/category/:id
+router.patch('/:id', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.update);
 
 module.exports = router;
