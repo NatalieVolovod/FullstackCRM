02787f1e641a50ff7f5d44cfc607fdee3b7e01f9
NatalieVolovod <volovod.natalie@gmail.com>
2020-04-18 21:17:02 +0300

    added user authentication

--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const controller = require('../controllers/order');
 
 // localhost:5000/api/order
-router.get('/', controller.getAll);
+router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
 
 // localhost:5000/api/order
-router.post('/', controller.create);
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create);
 
 module.exports = router;
--- routes/position.js
+++ routes/position.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const controller = require('../controllers/position');
 
 // localhost:5000/api/position/:categoryId
-router.get('/:categoryId', controller.getByCategoryId);
+router.get('/:categoryId', passport.authenticate('jwt', {session: false}), controller.getByCategoryId);
 
 // localhost:5000/api/position
-router.post('/', controller.create);
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create);
 
 // localhost:5000/api/position/:id
-router.patch('/:id', controller.update);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), controller.update);
 
 // localhost:5000/api/position/:id
-router.delete('/:id', controller.remove);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
 
 module.exports = router;
