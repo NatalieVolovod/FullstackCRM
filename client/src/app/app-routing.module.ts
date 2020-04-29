cded20840e605550daa0fb82fa5a0dcd0cfd39a4
NatalieVolovod <volovod.natalie@gmail.com>
2020-04-29 15:37:00 +0300

    added registration component, login and registration routes, authorization and core application layouts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
+import {AppLayoutComponent} from "./shared/layouts/app-layout/app-layout.component";
 import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 
 const routes: Routes = [
-  {path:'login', component: LoginComponent}
+  {
+    path:'', component: AuthLayoutComponent, children: [
+      {path:'', redirectTo: '/login', pathMatch: 'full'},
+      {path: 'login', component: LoginComponent},
+      {path: 'registration', component: RegisterComponent}
+    ]
+  },
+  {
+    path:'', component: AppLayoutComponent, children: [
+
+    ]
+  }
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { AppLayoutComponent } from './shared/layouts/app-layout/app-layout.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AuthLayoutComponent,
+    AppLayoutComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
